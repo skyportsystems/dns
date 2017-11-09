e57049aba4bff52984ae0454b81c69bf5206af85
Miek Gieben <miek@miek.nl>
2017-11-09 17:34:14 +0000

    Add fuzzing framework
    Add easy way to fuzz this dns library.

---
+++ fuzz.go
@@ -0,0 +1,46 @@
+// +build fuzz
+
+package dns
+
+import (
+	"bytes"
+	"encoding/hex"
+	"os"
+)
+
+func Fuzz(rawMsg []byte) int {
+	var (
+		msg         = &Msg{}
+		buf, bufOne = make([]byte, 100000), make([]byte, 100000)
+		res, resOne []byte
+
+		unpackErr, packErr error
+	)
+
+	if unpackErr = msg.Unpack(rawMsg); unpackErr != nil {
+		return 0
+	}
+
+	if res, packErr = msg.PackBuffer(buf); packErr != nil {
+		return 0
+	}
+
+	for i := range res {
+		bufOne[i] = 1
+	}
+
+	resOne, packErr = msg.PackBuffer(bufOne)
+	if packErr != nil {
+		println("Pack failed only with a filled buffer")
+		panic(packErr)
+	}
+
+	if !bytes.Equal(res, resOne) {
+		println("buffer bits leaked into the packed message")
+		println(hex.Dump(res))
+		println(hex.Dump(resOne))
+		os.Exit(1)
+	}
+
+	return 1
+}
