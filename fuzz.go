fa01a149e694c306309108855c53fc60ff597220
Miek Gieben <miek@miek.nl>
2017-11-10 11:02:36 +0000

    errors are not interesting

--- fuzz.go
+++ fuzz.go
@@ -3,17 +3,17 @@
 package dns
 
 func Fuzz(data []byte) int {
-	ret := 0
+	ret := 1
 	msg := new(Msg)
 
 	err := msg.Unpack(data)
 	if err != nil {
-		ret = 1
+		ret = 0
 	}
 
 	_, err = msg.Pack()
 	if err != nil {
-		ret = 1
+		ret = 0
 	}
 
 	return ret
