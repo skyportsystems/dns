53149ea7edf9f19bad710f3ef3ca1d8fa632869f
Miek Gieben <miek@miek.nl>
2017-11-10 10:44:25 +0000

    simpler fuzzer - add more to corpus

--- fuzz.go
+++ fuzz.go
@@ -2,45 +2,19 @@
 
 package dns
 
-import (
-	"bytes"
-	"encoding/hex"
-	"os"
-)
-
 func Fuzz(data []byte) int {
-	var (
-		msg         = &Msg{}
-		buf, bufOne = make([]byte, 100000), make([]byte, 100000)
-		res, resOne []byte
+	ret := 0
+	msg := new(Msg)
 
-		unpackErr, packErr error
-	)
-
-	if unpackErr = msg.Unpack(data); unpackErr != nil {
-		return 0
+	err := msg.Unpack(data)
+	if err != nil {
+		ret = 1
 	}
 
-	if res, packErr = msg.PackBuffer(buf); packErr != nil {
-		return 0
+	_, err = msg.Pack()
+	if err != nil {
+		ret = 1
 	}
 
-	for i := range res {
-		bufOne[i] = 1
-	}
-
-	resOne, packErr = msg.PackBuffer(bufOne)
-	if packErr != nil {
-		println("pack failed only with a filled buffer")
-		panic(packErr)
-	}
-
-	if !bytes.Equal(res, resOne) {
-		println("buffer bits leaked into the packed message")
-		println(hex.Dump(res))
-		println(hex.Dump(resOne))
-		os.Exit(1)
-	}
-
-	return 1
+	return ret
 }
